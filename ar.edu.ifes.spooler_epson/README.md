f6b2ba589781249cd99d76d5890293c0a6e6a6c4
Diego Cabrera <dscabrera@gmail.com>
2012-11-07 19:43:13 -0300

    borrando carpeta duplicada

--- ar.edu.ifes.spooler_epson/README.md
+++
@@ -1,2 +0,0 @@
-ar.edu.ifes.spooler_epson
-=========================
--- ar.edu.ifes.spooler_epson/src/main/java/ifepson/Patagonia.java
+++
@@ -1,9 +0,0 @@
-package ifepson;
-
-public class Patagonia {
-	
-	  public static void main(String[] args) {
-	      System.out.println("Hola github");
-	
-	  }
-}
